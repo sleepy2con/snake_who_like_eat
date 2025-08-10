8a8375c310533ad9fced63bcc23e81b49fdb53ac
hyx <heyuxuan3985@outlook.com>
2025-08-10 17:44:04 +0800

    尝试修改食物会显示在地图网格边界处，可能同时在两三个网格中的问题

--- snake_who_like_eat/include/constant.h
+++ snake_who_like_eat/include/constant.h
@@ -5,7 +5,7 @@ constexpr int TILE_SIZE = 10;
 constexpr int SCALE_UNIT_SIZE = 50;
 
 // 设置地图的宽高,宽为SCALE_UNIT_SIZE * WIDTH_RATIO,诸如此类
-constexpr int WIDTH_RATIO = 16;
+constexpr int WIDTH_RATIO = 8;
 constexpr int HEIGHT_RATIO = 9;
 
 constexpr unsigned int OneFoodPoints = 10;
--- snake_who_like_eat/src/food.cpp
+++ snake_who_like_eat/src/food.cpp
@@ -4,21 +4,21 @@
 
 static const qreal FOOD_RADIUS = 3.0;
 
-Food::Food(qreal x, qreal y)
-{
-	setPos(x, y);
-	setData(static_cast<int>(GameObjectsData::GD_Type), QVariant::fromValue(GameObjectTypes::GO_Food));
+Food::Food(qreal x, qreal y) {
+    // TODO 食物会显示在地图网格边界处，可能同时在两三个网格中，考虑修改下
+    setPos(x, y);
+    setData(static_cast<int>(GameObjectsData::GD_Type), QVariant::fromValue(GameObjectTypes::GO_Food));
 }
 
 QRectF Food::boundingRect() const {
-	return {-TILE_SIZE, -TILE_SIZE, TILE_SIZE * 2, TILE_SIZE * 2}; // Example size, adjust as needed
+    return {-TILE_SIZE / 2, -TILE_SIZE / 2, TILE_SIZE - 2, TILE_SIZE - 2}; // Example size, adjust as needed
 }
 
-void Food::paint(QPainter* p_, const QStyleOptionGraphicsItem*, QWidget*) {
-	p_->setBrush(Qt::red); // Set the color of the food
-	//p_->drawEllipse(boundingRect()); // Draw a circle for the foodi 
-	p_->setRenderHint(QPainter::Antialiasing, true); // Enable antialiasing for smoother edges
-	QPainterPath path_;
-	path_.addEllipse(QPointF(TILE_SIZE / 2, TILE_SIZE / 2), FOOD_RADIUS, FOOD_RADIUS);
-	p_->fillPath(path_, Qt::red);
+void Food::paint(QPainter *p_, const QStyleOptionGraphicsItem *, QWidget *) {
+    p_->setBrush(Qt::red); // Set the color of the food
+    //p_->drawEllipse(boundingRect()); // Draw a circle for the foodi
+    p_->setRenderHint(QPainter::Antialiasing, true); // Enable antialiasing for smoother edges
+    QPainterPath path_;
+    path_.addEllipse(QPointF(0, 0), FOOD_RADIUS, FOOD_RADIUS);
+    p_->fillPath(path_, Qt::red);
 }
